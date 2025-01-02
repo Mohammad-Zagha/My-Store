8f1abb1dce45d24425c3f977ef7ff75ee0ab7abc
Mohammad Zagha <tsu.test11@gmail.com>
2025-01-02 22:55:35 +0200

    update axios

--- lib/Axios.tsx
+++ lib/Axios.tsx
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios'
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001/api'
+const baseURL = process.env.NEXT_PUBLIC_API_URL ?? 'https://perfume-shop-backend.onrender.com/api'
 
 function createAxiosInstance({ withAuth }: { withAuth: boolean } = { withAuth: false }): AxiosInstance {
    const axiosInstance = axios.create({
