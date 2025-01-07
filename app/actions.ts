c94a9438c0e1da2b52bb36a4a04ed3e8310e7ae2
Mohammad Zagha <tsu.test11@gmail.com>
2025-01-07 20:36:43 +0200

    cookie test

--- app/actions.ts
+++ app/actions.ts
@@ -1,10 +1,9 @@
    import { cookies } from "next/headers";
-   import { setCookie, deleteCookie, getCookie, getCookies, hasCookie } from 'cookies-next';
 
    export async function decodeToken()
    {
    try {
-      const accessToken = getCookie('AccessToken');
+      const accessToken = cookies().get('AccessToken')?.value || undefined;
       return accessToken
       
    } catch (error) {
